60d68852126c99dbe4756cd6f57adf6c26684300
Sounava Mukherjee <soumo2640@gmail.com>
2026-05-16 19:10:42 +0530

    message

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { TypeAnimation } from 'react-type-animation';
-import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/profile-img.png';
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
+import Tilt from "react-parallax-tilt";
+import profileImage from "../../assets/profile-img.png";
 
 const About = () => {
   return (
@@ -15,46 +15,45 @@ const About = () => {
           {/* Greeting */}
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-2 leading-tight">
             Hi, I am
-          </h1> 
+          </h1>
           <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
-           Sounava Mukherjee
-          </h2> 
+            Sounava Mukherjee
+          </h2>
           <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
             <span className="text-white">I am a </span>
             <TypeAnimation
-  sequence={[
-    'Fullstack Developer',
-    1000,
-    'Mern Developer',
-    1000,
-    'Coder',
-    1000,
-  ]}
-  wrapper="span"
-  speed={50}
-  repeat={Infinity}
-/>
-          </h3> 
+              sequence={[
+                "Fullstack Developer",
+                1000,
+                "Mern Developer",
+                1000,
+                "Coder",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h3>
           <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
-            a full-stack developer with over 1 year of experience in
-            building scalable web applications. Skilled in both front-end and
-            back-end development, I specialize in the MERN stack and other
-            modern technologies to create seamless user experiences and
-            efficient solutions.
-          </p> 
+            a full-stack developer with over 1 year of experience in building
+            scalable web applications. Skilled in both front-end and back-end
+            development, I specialize in the MERN stack and other modern
+            technologies to create seamless user experiences and efficient
+            solutions.
+          </p>
           <a
-            href=" "
+            href="https://drive.google.com/file/d/1JhPuhglwQl77xtGpMLkVh8MtBXTVqDeM/view"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
             style={{
-              background: 'linear-gradient(90deg, #8245ec, #a855f7)',
-              boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
+              background: "linear-gradient(90deg, #8245ec, #a855f7)",
+              boxShadow: "0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec",
             }}
           >
             DOWNLOAD CV
           </a>
-          
         </div>
         {/* Right Side */}
         <div className="md:w-1/2 flex justify-center md:justify-end">
